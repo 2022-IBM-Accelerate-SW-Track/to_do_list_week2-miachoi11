e6567dd705ea8139de6074c98b03508e2db444d5
Mia Choi <miaminchoe@gmail.com>
2022-06-20 14:08:39 -0500

    updated aboutme

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import aboutMePhoto from "../assets/aboutMePhoto.jpg";
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
+      <div class="split left">
+        <div className="centered">
+          <img 
+            className="profile_image"
+            src={aboutMePhoto}
+            alt="Profile Pic"
             ></img>
-          </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Mia Choi</div>
+          <div className="brief_description">
+            Hi! My name is Mia and I'm an upcoming junior at Johns Hopkins University double majoring in Computer Science and Public Health. I am also interning this summer at an ed-tech startup called Phenomena!
+          </div>
+          <div className="hobbies">
+            In my spare time I love learning how to build things with code, playing video games, listening to psychology podcasts, and watching videos of cute animals on Youtube! 
           </div>
         </div>
       </div>
-    );
+    </div>
+    )
   }
 }
